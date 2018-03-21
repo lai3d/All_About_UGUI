e7fea6ea7729047197d4dd104e4b5b5ec9eff178
Larry Lai <larry@qjt.sg>
2018-03-21 10:10:15 +0800

    Ruler DrawAxis

---
+++ Assets/SpringGUI/Demo/11_Ruler/RulerExample.cs
@@ -0,0 +1,18 @@
+﻿
+/*=========================================
+* Author: springDong
+* Description: SpringGUI.LineChartGraph example.
+==========================================*/
+
+using System.Collections.Generic;
+using SpringGUI;
+using UnityEngine;
+
+public class RulerExample : MonoBehaviour
+{
+    public Ruler ruler = null;
+
+    public void Awake()
+    {
+    }
+}
