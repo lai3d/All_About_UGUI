b51617fb3fa304ff145a3969e161dbb42c95cf19
Larry Lai <larry@qjt.sg>
2018-05-02 11:38:53 +0800

    Mouse Cursor controller, both 3d and ugui

---
+++ Assets/Scripts/MouseCursorUguiController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MouseCursorUguiController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    public void OnPointerEnter (PointerEventData eventData) {
+        Debug.Log ("OnPointerEnter");
+        Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);
+    }
+
+    public void OnPointerExit (PointerEventData eventData) {
+        Debug.Log ("OnPointerExit");
+        Cursor.SetCursor (null, Vector2.zero, cursorMode);
+    }
+}
